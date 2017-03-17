3af4f74b4f79b43aa6116e228ec2c62e5431d359
Alan Martins Vargas <metaldesigner2@gmail.com>
2017-03-17 12:01:08 -0300

    Add files via upload
    Subindo arquivos

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+
+
+
+module.exports = {
+  entry: "src/index.js",
+  output: {
+    path: '/public',
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  }
+}
+  
+  
+}
